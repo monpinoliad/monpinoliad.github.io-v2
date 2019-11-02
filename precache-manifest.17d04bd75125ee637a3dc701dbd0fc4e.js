9c0cc0575a3c4c37fb89110b5cfc480b4d5278d9
monpinoliad <monpinoliad@gmail.com>
2019-11-02 09:14:23 +0800

    Added v2.0.2

---
+++ precache-manifest.17d04bd75125ee637a3dc701dbd0fc4e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "974b8c1d877e9f56654a3bb3d7f84ee7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2346e0c521f29235045",
+    "url": "/static/css/main.6af10c0f.chunk.css"
+  },
+  {
+    "revision": "4998c41f4512ddb138d1",
+    "url": "/static/js/2.b0dfc117.chunk.js"
+  },
+  {
+    "revision": "d2346e0c521f29235045",
+    "url": "/static/js/main.5905c8b2.chunk.js"
+  },
+  {
+    "revision": "ae970b72d1121cf53cb6",
+    "url": "/static/js/runtime-main.3664b334.js"
+  },
+  {
+    "revision": "3412fc8fcd88eeed7e49832a0b1f5414",
+    "url": "/static/media/PINOLIAD_RESUME_REAL_october28-19.3412fc8f.pdf"
+  },
+  {
+    "revision": "89e02d41446b73046edf09d934441f8a",
+    "url": "/static/media/close-envelope.89e02d41.svg"
+  },
+  {
+    "revision": "f04ff9cbe924910eecd59c1355ab5a7a",
+    "url": "/static/media/confident.f04ff9cb.svg"
+  },
+  {
+    "revision": "11df930daca906939c8480355932298e",
+    "url": "/static/media/dynamic.11df930d.svg"
+  },
+  {
+    "revision": "e1ba864fd66b7613d824e00762628869",
+    "url": "/static/media/elpower.e1ba864f.png"
+  },
+  {
+    "revision": "944a4dd6ab98ae7df9d4d274af1e8a97",
+    "url": "/static/media/github-logo.944a4dd6.svg"
+  },
+  {
+    "revision": "c6f60b42ed044ff8c5635c5fafddb1dc",
+    "url": "/static/media/instagram.c6f60b42.svg"
+  },
+  {
+    "revision": "a4749764f1db9c1d5737203c577310a7",
+    "url": "/static/media/jimmy.a4749764.png"
+  },
+  {
+    "revision": "a1baa9e9034ba7cd3534f29718c5e7b9",
+    "url": "/static/media/linkedin-logo.a1baa9e9.svg"
+  },
+  {
+    "revision": "c05a87a4e871e6836129b0b721a9f4a7",
+    "url": "/static/media/motivated.c05a87a4.svg"
+  },
+  {
+    "revision": "cac3413337cf80757d7c106bbe95b770",
+    "url": "/static/media/rey.cac34133.png"
+  },
+  {
+    "revision": "c28c6a7a442391a9a3242f504085b957",
+    "url": "/static/media/smdi.c28c6a7a.png"
+  }
+]);
